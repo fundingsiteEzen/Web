1a0f14e38af3ef06e85ebc0297a69d298535ed5d
hatecho <terranljbsl@gmail.com>
2022-08-04 12:41:41 +0900

    1

---
+++ src/main/java/com/fun/myPage/Controller/MyController.java
@@ -0,0 +1,19 @@
+package com.fun.myPage.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/myPage")
+public class MyController {
+	
+	
+	@RequestMapping(value="/fundingMy", method=RequestMethod.GET)
+	public void projectList(Model model) throws Exception {
+		
+	
+	}
+	
+}
